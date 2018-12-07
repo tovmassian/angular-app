48c4b65590bb585828622c3303c204b20346c49a
Khachatur Tovmasyan <khachatur.tovmasyan@sflpro.com>
2018-12-07 10:26:08 +0400

    linting config, sass setup, product-list componnet implementation, models and interfaces config and components restructure
    travis CI integration

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'pm-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+	selector: 'pm-root',
+	template: `
+		<div>
+			<h1>Welcome to {{pageTitle}}!!</h1>
+			<pm-products></pm-products>
+		</div>`,
+	styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'Angular: Getting Started';
+	pageTitle = 'Angular: Getting Started';
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ProductListComponent } from './components/products/product-list.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  bootstrap: [AppComponent]
+	imports: [
+		BrowserModule,
+		FormsModule
+	],
+	declarations: [
+		AppComponent,
+		ProductListComponent,
+	],
+	bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
