e9fc215baf0c6476e791bf9e9706205f8c1ffef5
Khachatur Tovmasyan <khachatur.tovmasyan@sflpro.com>
2018-12-07 10:49:01 +0400

    custom pipe implementation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,8 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { ProductListComponent } from './components/products/product-list.component';
 
+import { ConvertToSpacesPipe } from './pipes/convert-to-spaces.pipe';
+
 @NgModule({
 	imports: [
 		BrowserModule,
@@ -13,6 +15,7 @@ import { ProductListComponent } from './components/products/product-list.compone
 	declarations: [
 		AppComponent,
 		ProductListComponent,
+		ConvertToSpacesPipe,
 	],
 	bootstrap: [AppComponent]
 })
